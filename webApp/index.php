e012ae30624daa70ecc518eb84e5eba70cedbce1
Ansame1 <135002807+Ansame1@users.noreply.github.com>
2023-06-12 17:09:21 +0200

    var

---
+++ webApp/index.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+<div style="width: 100%; font-size: 40px; font-weight: bold; text-align: center;">
+UserDir Test Page
+</div>
+
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "learnagement";
+$port = 43306;
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+</body>
+</html>
