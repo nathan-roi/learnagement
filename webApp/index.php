c10e7c14fd760db3ff59ab70d63c2f95755b0485
Ansame1 <135002807+Ansame1@users.noreply.github.com>
2023-06-14 09:10:09 +0200

    Refonte du code php

--- webApp/index.php
+++ webApp/index.php
@@ -18,7 +18,7 @@ $conn = new mysqli($servername, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully1";
 ?>
 
 </body>
