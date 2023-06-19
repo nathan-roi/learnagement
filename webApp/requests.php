52d2d3d01818ef31d34a3d873dd3a2a3185109dc
Flavien Vernier <flavien.vernier@gmail.com>
2023-06-20 00:05:03 +0200

    add webApp on main forgoten file

--- webApp/requests.php
+++ webApp/requests.php
@@ -1,8 +1,21 @@
+<?php
 
-$vues_req = "SELECT TABLE_NAME FROM information_schema.tables WHERE TABLE_TYPE = \"VIEW\" AND TABLE_NAME LIKE \"INFO_vue_%\";";
+// get parameters according to the session
+$param_req = "SELECT * FROM `INFO_vue_parameters` WHERE `sessionId` =  \"$sessionId\"";
 
-$tables_req = "SELECT TABLE_NAME FROM information_schema.tables WHERE TABLE_TYPE = \"BASE TABLE\" AND TABLE_NAME LIKE \"INFO_%\";";
+// get all views
+$vues_req = "SELECT TABLE_NAME FROM information_schema.tables WHERE TABLE_TYPE = \"VIEW\" AND TABLE_NAME LIKE \"INFO_vue_%\"";
 
-$secondaryK_req = "SHOW INDEXES FROM `INFO_enseignant` WHERE `Key_name`=\"SECONDARY\";";
+// get view column names
+$columns_vue_req = "SHOW COLUMNS FROM $vue_name";
 
-$foreignK_req = 
+// get view content
+$vue_req = " SELECT * FROM  $vue_name WHERE 1 ";
+
+// get all tables
+$tables_req = "SELECT TABLE_NAME FROM information_schema.tables WHERE TABLE_TYPE = \"BASE TABLE\" AND TABLE_NAME LIKE \"INFO_%\"";
+
+// get explicit secondary Key
+$secondaryK_req = "SHOW INDEXES FROM $table_name WHERE `Key_name`=\"SECONDARY\"";
+
+?>
