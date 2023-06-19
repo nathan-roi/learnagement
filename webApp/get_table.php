f3e44db0958140c3812a6f5381cf1a37971f4b93
Flavien Vernier <flavien.vernier@gmail.com>
2023-06-20 00:04:16 +0200

    add webApp on main

---
+++ webApp/get_table.php
@@ -0,0 +1,63 @@
+<?php
+ include("requests.php");
+
+print("<button type=\"button\" class=\"collapsible\">$vue_name</button>");
+
+print("<div class=\"content\">
+  <table>
+    <thead>");
+
+
+/*  on suppose, bien entendu, que la vue existe  */
+
+$result = mysqli_query($conn, $columns_vue_req);
+if (!$result) {
+  echo 'Impossible d\'exécuter la requête : ' . $req;
+  echo 'error '.mysqli_error();
+  exit;
+}
+
+if (mysqli_num_rows($result) > 0) {
+print("<tr>");
+   while ($row = mysqli_fetch_row($result)) {
+      print("<th>".$row[0]."</th>");
+   }
+   print("<th>Validation</th>");
+   print("</tr></thead>\n");
+}
+
+    mysqli_free_result($result); // libère l'espace mémoire occupé par le résultat
+
+    print("    <tbody>");
+    // envoi de la requête au serveur qui retourne un résultat    
+
+    $result  =   mysqli_query($conn, $vue_req);  
+    
+    if ($result === FALSE){
+      echo "la requ&ecirc;te a &eacute;chou&eacute; : ".mysqli_error();
+      exit; // inutile de poursuivre le traitement dans ce cas
+    } 
+    
+   while($ligne = mysqli_fetch_row ($result)){
+   	print("<tr>");
+	print("<form action='' method='get' class='form-row'>");
+   	foreach($ligne as $k=>$v){
+	    //print("<td>".$v."</td>");
+	    print("<td><input type='text' name='$k' value='$v'></td>");
+	}
+	print("<td><input type='submit' value='valid'></td>");
+	print("</form>");
+   	print("</tr>\n");
+   }
+print("
+    </tbody>
+  </table>
+</div>");
+
+   mysqli_free_result($result); // libère l'espace mémoire occupé par le résultat 
+
+ //  }else{
+//	print("Vue not found!");
+//   }
+
+?>   
---
+++ webApp/modif.php
@@ -0,0 +1,119 @@
+<?php
+    // Initialiser la session
+    session_start();
+    require_once("config.php");
+    if (isset($_SESSION['start']) && (time() - $_SESSION['start'] > $session_timeout)) {
+       session_unset(); 
+       session_destroy(); 
+       echo "you were disconnected due to a session timeout"; 
+    }
+    $_SESSION['start'] = time();
+
+    // Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
+    if(!isset($_SESSION["username"])){
+        /*header("Location: registration/login.php");
+        exit();*/
+        $username="anonymous";
+    }else{
+        $username=$_SESSION["username"];
+    }
+
+    require("connectDB.php");
+    $sessionId = session_id();
+    //print("-".$sessionId.".");
+
+    require("requests.php");
+
+    
+    $result = mysqli_query($conn, $param_req);
+    if (!$result) {
+       echo 'Impossible d\'exécuter la requête : ' . $req;
+       echo 'error '.mysqli_error();
+       exit;
+    }
+    if (mysqli_num_rows($result) > 0) {
+       $row = mysqli_fetch_assoc($result);
+       $semestre = $row["semestre"];
+       $module = $row["module"];
+       $enseignant = $row["enseignant"];
+    }else{
+	if(!isset($semestre)){
+	       $semestre = "";
+       	       $module = "";
+       	       $enseignant = "";
+	}
+    }
+
+
+    print("<!DOCTYPE html>
+        <html lang=\"fr\">
+            <head>
+                <link rel=\"stylesheet\" href=\"style.css\" />
+            </head>
+            <body>
+                <div class=\"sucess\">
+                    <h1>Bienvenue $username !</h1>
+                </div>
+                <div class=\"paramview\">
+                    <form action=\"setViewParameters.php\" method=\"post\">
+		    	<input type=\"hidden\" name=\"sessionId\" value=\"$sessionId\" readonly>
+                        <div class=\"form-group\">
+                            <label>Semestre</label>
+                            <input type=\"text\" name=\"semestre\" class=\"form-control\" value=$semestre>
+                        </div>
+                        <div class=\"form-group\">
+                            <label>Module</label>
+                            <input type=\"text\" name=\"module\" class=\"form-control\" value=$module>
+                        </div>
+                        <div class=\"form-group\">
+                            <label>Enseignant</label>
+                            <input type=\"text\" name=\"enseignant\" class=\"form-control\" value=$enseignant>
+                        </div>
+                        <input type=\"submit\" class=\"btn btn-primary\" name=\"submit\" value=\"Submit\">
+                    </form>
+                </div>               
+    ");
+
+
+    $views = mysqli_query($conn, $tables_req);
+    if (!$views) {
+       echo 'Impossible d\'exécuter la requête : ' . $req;
+       echo 'error '.mysqli_error();
+       exit;
+     }
+     while ($view = mysqli_fetch_row($views)) {
+     	   $vue_name = $view[0];
+    	   include("get_table.php");
+     }
+     require("disconnectDB.php");
+
+    if($username != "anonymous"){
+        print("<a href=\"registration/logout.php\">Déconnexion</a>");
+    }else{
+        print("<a href=\"registration/login.php\">Connexion</a>");
+    }
+
+print("
+    <script>
+        var coll = document.getElementsByClassName(\"collapsible\");
+        var i;
+
+        for (i = 0; i < coll.length; i++) {
+            coll[i].addEventListener(\"click\", function() {
+                this.classList.toggle(\"active\");
+                var content = this.nextElementSibling;
+                if (content.style.display === \"block\") {
+                    content.style.display = \"none\";
+                } else {
+                    content.style.display = \"block\";
+                }
+            });
+	}
+	</script>
+    ");
+    
+    print("
+            </body>
+        </html>
+    ");
+?>
