883459a0713f1cc646d9b9a74ed340707e1030a9
Flavien Vernier <flavien.vernier@gmail.com>
2024-11-28 11:49:41 +0100

    debug BD table name update

--- db/sql/0_learnagement.sql
+++ db/sql/0_learnagement.sql
@@ -396,38 +396,6 @@ CREATE TABLE `MAQUETTE_module_sequencage` (
   `modifiable` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Déclencheurs `MAQUETTE_module_sequencage`
---
-DELIMITER $$
-CREATE TRIGGER `generate_sequence_from_sequencage` AFTER INSERT ON `MAQUETTE_module_sequencage` FOR EACH ROW INSERT IGNORE INTO `INFO_module_sequence`  (`id_module_sequencage`,`numero_ordre`,`commentaire`)
-SELECT `id_module_sequencage`, orders, null
-FROM `INFO_module_sequencage`, 
-(VALUES ROW (1), ROW(2), ROW(3), ROW (4), ROW(5), ROW(6), ROW (7), ROW(8), ROW(9), 
- ROW (10), ROW (11), ROW(12), ROW(13), ROW (14), ROW(15), ROW(16), ROW (17), ROW(18), ROW(19),
- ROW (20), ROW (21), ROW(22), ROW(23), ROW (24), ROW(25), ROW(26), ROW (27), ROW(28), ROW(29),
- ROW (30), ROW (31), ROW(32), ROW(33), ROW (34), ROW(35), ROW(36), ROW (37), ROW(38), ROW(39)  ) sub(orders)
-WHERE orders <= nombre
-$$
-DELIMITER ;
-DELIMITER $$
-CREATE TRIGGER `generate_type_seance_from_sequencage` AFTER INSERT ON `MAQUETTE_module_sequencage` FOR EACH ROW INSERT IGNORE INTO INFO_type_seance_to_be_affected (`id_module_sequencage`, `id_groupe`)
-(
-SELECT
-  `learnagement`.`INFO_module_sequencage`.`id_module_sequencage` AS `id_module_sequencage`,
-  `learnagement`.`INFO_groupe`.`id_groupe` AS `id_groupe`
-  
-  FROM `learnagement`.`INFO_module`
-
-  JOIN INFO_module_as_learning_unit ON INFO_module_as_learning_unit.id_module = INFO_module.id_module
-  JOIN INFO_learning_unit ON INFO_learning_unit.id_learning_unit = INFO_module_as_learning_unit.id_learning_unit
-  JOIN INFO_promo ON INFO_learning_unit.id_promo = INFO_promo.id_promo
-  
-  JOIN `learnagement`.`INFO_groupe` ON `learnagement`.`INFO_promo`.`id_promo` = `learnagement`.`INFO_groupe`.`id_promo`
-  JOIN `learnagement`.`INFO_module_sequencage` ON (`learnagement`.`INFO_module`.`id_module` = `learnagement`.`INFO_module_sequencage`.`id_module`) AND (`learnagement`.`INFO_groupe`.`id_groupe_type` = `learnagement`.`INFO_module_sequencage`.`id_groupe_type`)
-)
-$$
-DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -442,27 +410,6 @@ CREATE TABLE `MAQUETTE_module_sequence` (
   `commentaire` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Déclencheurs `MAQUETTE_module_sequence`
---
-DELIMITER $$
-CREATE TRIGGER `generate_seance_from_sequence` AFTER INSERT ON `MAQUETTE_module_sequence` FOR EACH ROW INSERT IGNORE INTO INFO_seance_to_be_affected (`id_module_sequence`, `id_groupe`)
-SELECT
-  `learnagement`.`INFO_module_sequence`.`id_module_sequence` AS `id_module_sequence`,
-  `learnagement`.`INFO_groupe`.`id_groupe` AS `id_groupe`
-  
-  FROM `INFO_module`
-
-  JOIN INFO_module_as_learning_unit ON INFO_module_as_learning_unit.id_module = INFO_module.id_module
-  JOIN INFO_learning_unit ON INFO_learning_unit.id_learning_unit = INFO_module_as_learning_unit.id_learning_unit
-  JOIN INFO_promo ON INFO_learning_unit.id_promo = INFO_promo.id_promo
-  
-  JOIN `INFO_groupe` ON `INFO_promo`.`id_promo` = `INFO_groupe`.`id_promo`
-  
-  JOIN `INFO_module_sequencage` ON (`INFO_module`.`id_module` = `INFO_module_sequencage`.`id_module`) AND (`INFO_groupe`.`id_groupe_type` = `INFO_module_sequencage`.`id_groupe_type`)
-  JOIN INFO_module_sequence ON INFO_module_sequence.id_module_sequencage = INFO_module_sequencage.id_module_sequencage
-$$
-DELIMITER ;
 
 -- --------------------------------------------------------
 
--- db/sql/99_triggers.sql
+++ db/sql/99_triggers.sql
@@ -1,43 +1,43 @@
-CREATE TRIGGER `generate_seance_from_sequence` AFTER INSERT ON `INFO_module_sequence`
- FOR EACH ROW INSERT IGNORE INTO INFO_seance_to_be_affected (`id_module_sequence`, `id_groupe`)
+CREATE TRIGGER `generate_seance_from_sequence` AFTER INSERT ON `MAQUETTE_module_sequence`
+ FOR EACH ROW INSERT IGNORE INTO CLASS_session_to_be_affected (`id_module_sequence`, `id_groupe`)
 SELECT
-  `learnagement`.`INFO_module_sequence`.`id_module_sequence` AS `id_module_sequence`,
-  `learnagement`.`INFO_groupe`.`id_groupe` AS `id_groupe`
+  `learnagement`.`MAQUETTE_module_sequence`.`id_module_sequence` AS `id_module_sequence`,
+  `learnagement`.`LNM_groupe`.`id_groupe` AS `id_groupe`
   
-  FROM `INFO_module`
+  FROM `MAQUETTE_module`
 
-  JOIN INFO_module_as_learning_unit ON INFO_module_as_learning_unit.id_module = INFO_module.id_module
-  JOIN INFO_learning_unit ON INFO_learning_unit.id_learning_unit = INFO_module_as_learning_unit.id_learning_unit
-  JOIN INFO_promo ON INFO_learning_unit.id_promo = INFO_promo.id_promo
+  JOIN MAQUETTE_module_as_learning_unit ON MAQUETTE_module_as_learning_unit.id_module = MAQUETTE_module.id_module
+  JOIN MAQUETTE_learning_unit ON MAQUETTE_learning_unit.id_learning_unit = MAQUETTE_module_as_learning_unit.id_learning_unit
+  JOIN LNM_promo ON MAQUETTE_learning_unit.id_promo = LNM_promo.id_promo
   
-  JOIN `INFO_groupe` ON `INFO_promo`.`id_promo` = `INFO_groupe`.`id_promo`
+  JOIN `LNM_groupe` ON `LNM_promo`.`id_promo` = `LNM_groupe`.`id_promo`
   
-  JOIN `INFO_module_sequencage` ON (`INFO_module`.`id_module` = `INFO_module_sequencage`.`id_module`) AND (`INFO_groupe`.`id_groupe_type` = `INFO_module_sequencage`.`id_groupe_type`)
-  JOIN INFO_module_sequence ON INFO_module_sequence.id_module_sequencage = INFO_module_sequencage.id_module_sequencage;
+  JOIN `MAQUETTE_module_sequencage` ON (`MAQUETTE_module`.`id_module` = `MAQUETTE_module_sequencage`.`id_module`) AND (`LNM_groupe`.`id_groupe_type` = `MAQUETTE_module_sequencage`.`id_groupe_type`)
+  JOIN MAQUETTE_module_sequence ON MAQUETTE_module_sequence.id_module_sequencage = MAQUETTE_module_sequencage.id_module_sequencage;
 
-CREATE TRIGGER `generate_sequence_from_sequencage` AFTER INSERT ON `INFO_module_sequencage`
- FOR EACH ROW INSERT IGNORE INTO `INFO_module_sequence`  (`id_module_sequencage`,`numero_ordre`,`commentaire`)
+CREATE TRIGGER `generate_sequence_from_sequencage` AFTER INSERT ON `MAQUETTE_module_sequencage`
+ FOR EACH ROW INSERT IGNORE INTO `MAQUETTE_module_sequence`  (`id_module_sequencage`,`numero_ordre`,`commentaire`)
 SELECT `id_module_sequencage`, orders, null
-FROM `INFO_module_sequencage`, 
+FROM `MAQUETTE_module_sequencage`, 
 (VALUES ROW (1), ROW(2), ROW(3), ROW (4), ROW(5), ROW(6), ROW (7), ROW(8), ROW(9), 
  ROW (10), ROW (11), ROW(12), ROW(13), ROW (14), ROW(15), ROW(16), ROW (17), ROW(18), ROW(19),
  ROW (20), ROW (21), ROW(22), ROW(23), ROW (24), ROW(25), ROW(26), ROW (27), ROW(28), ROW(29),
  ROW (30), ROW (31), ROW(32), ROW(33), ROW (34), ROW(35), ROW(36), ROW (37), ROW(38), ROW(39)  ) sub(orders)
 WHERE orders <= nombre;
 
-CREATE TRIGGER `generate_type_seance_from_sequencage` AFTER INSERT ON `INFO_module_sequencage`
- FOR EACH ROW INSERT IGNORE INTO INFO_type_seance_to_be_affected (`id_module_sequencage`, `id_groupe`)
+CREATE TRIGGER `generate_type_seance_from_sequencage` AFTER INSERT ON `MAQUETTE_module_sequencage`
+ FOR EACH ROW INSERT IGNORE INTO CLASS_session_type_to_be_affected (`id_module_sequencage`, `id_groupe`)
 (
 SELECT
-  `learnagement`.`INFO_module_sequencage`.`id_module_sequencage` AS `id_module_sequencage`,
-  `learnagement`.`INFO_groupe`.`id_groupe` AS `id_groupe`
+  `learnagement`.`MAQUETTE_module_sequencage`.`id_module_sequencage` AS `id_module_sequencage`,
+  `learnagement`.`LNM_groupe`.`id_groupe` AS `id_groupe`
   
-  FROM `learnagement`.`INFO_module`
+  FROM `learnagement`.`MAQUETTE_module`
 
-  JOIN INFO_module_as_learning_unit ON INFO_module_as_learning_unit.id_module = INFO_module.id_module
-  JOIN INFO_learning_unit ON INFO_learning_unit.id_learning_unit = INFO_module_as_learning_unit.id_learning_unit
-  JOIN INFO_promo ON INFO_learning_unit.id_promo = INFO_promo.id_promo
+  JOIN MAQUETTE_module_as_learning_unit ON MAQUETTE_module_as_learning_unit.id_module = MAQUETTE_module.id_module
+  JOIN MAQUETTE_learning_unit ON MAQUETTE_learning_unit.id_learning_unit = MAQUETTE_module_as_learning_unit.id_learning_unit
+  JOIN LNM_promo ON MAQUETTE_learning_unit.id_promo = LNM_promo.id_promo
   
-  JOIN `learnagement`.`INFO_groupe` ON `learnagement`.`INFO_promo`.`id_promo` = `learnagement`.`INFO_groupe`.`id_promo`
-  JOIN `learnagement`.`INFO_module_sequencage` ON (`learnagement`.`INFO_module`.`id_module` = `learnagement`.`INFO_module_sequencage`.`id_module`) AND (`learnagement`.`INFO_groupe`.`id_groupe_type` = `learnagement`.`INFO_module_sequencage`.`id_groupe_type`)
+  JOIN `learnagement`.`LNM_groupe` ON `learnagement`.`LNM_promo`.`id_promo` = `learnagement`.`LNM_groupe`.`id_promo`
+  JOIN `learnagement`.`MAQUETTE_module_sequencage` ON (`learnagement`.`MAQUETTE_module`.`id_module` = `learnagement`.`MAQUETTE_module_sequencage`.`id_module`) AND (`learnagement`.`LNM_groupe`.`id_groupe_type` = `learnagement`.`MAQUETTE_module_sequencage`.`id_groupe_type`)
 )
