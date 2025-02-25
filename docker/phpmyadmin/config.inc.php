d6cfccf975e1b200c658b5ddb404e5c8539465da
FIMAlexSender <FIM.Alex.Sender@gmail.com>
2025-02-25 17:18:26 +0100

    Modify pers

--- docker/phpmyadmin/config.inc.php
+++ docker/phpmyadmin/config.inc.php
@@ -59,19 +59,19 @@ if (isset($_ENV['PMA_ABSOLUTE_URI'])) {
 $hosts = ['db'];
 
 /* Set by environment */
-if (! empty($_ENV['PMA_HOST'])) {
+if (!empty($_ENV['PMA_HOST'])) {
     $hosts = [$_ENV['PMA_HOST']];
     $verbose = [$_ENV['PMA_VERBOSE']];
     $ports = [$_ENV['PMA_PORT']];
-} elseif (! empty($_ENV['PMA_HOSTS'])) {
+} elseif (!empty($_ENV['PMA_HOSTS'])) {
     $hosts = array_map('trim', explode(',', $_ENV['PMA_HOSTS']));
     $verbose = array_map('trim', explode(',', $_ENV['PMA_VERBOSES']));
     $ports = array_map('trim', explode(',', $_ENV['PMA_PORTS']));
 }
 
-if (! empty($_ENV['PMA_SOCKET'])) {
+if (!empty($_ENV['PMA_SOCKET'])) {
     $sockets = [$_ENV['PMA_SOCKET']];
-} elseif (! empty($_ENV['PMA_SOCKETS'])) {
+} elseif (!empty($_ENV['PMA_SOCKETS'])) {
     $sockets = explode(',', $_ENV['PMA_SOCKETS']);
 }
 
@@ -92,38 +92,38 @@ for ($i = 1; isset($hosts[$i - 1]); $i++) {
         $cfg['Servers'][$i]['auth_type'] = 'cookie';
     }
     if (isset($_ENV['PMA_PMADB'])) {
-      $cfg['Servers'][$i]['pmadb'] = $_ENV['PMA_PMADB'];
-      $cfg['Servers'][$i]['relation'] = 'pma__relation';
-      $cfg['Servers'][$i]['table_info'] = 'pma__table_info';
-      $cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
-      $cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
-      $cfg['Servers'][$i]['column_info'] = 'pma__column_info';
-      $cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
-      $cfg['Servers'][$i]['history'] = 'pma__history';
-      $cfg['Servers'][$i]['recent'] = 'pma__recent';
-      $cfg['Servers'][$i]['favorite'] = 'pma__favorite';
-      $cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
-      $cfg['Servers'][$i]['tracking'] = 'pma__tracking';
-      $cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
-      $cfg['Servers'][$i]['users'] = 'pma__users';
-      $cfg['Servers'][$i]['usergroups'] = 'pma__usergroups';
-      $cfg['Servers'][$i]['navigationhiding'] = 'pma__navigationhiding';
-      $cfg['Servers'][$i]['savedsearches'] = 'pma__savedsearches';
-      $cfg['Servers'][$i]['central_columns'] = 'pma__central_columns';
-      $cfg['Servers'][$i]['designer_settings'] = 'pma__designer_settings';
-      $cfg['Servers'][$i]['export_templates'] = 'pma__export_templates';
+        $cfg['Servers'][$i]['pmadb'] = $_ENV['PMA_PMADB'];
+        $cfg['Servers'][$i]['relation'] = 'pma__relation';
+        $cfg['Servers'][$i]['table_info'] = 'pma__table_info';
+        $cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
+        $cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
+        $cfg['Servers'][$i]['column_info'] = 'pma__column_info';
+        $cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
+        $cfg['Servers'][$i]['history'] = 'pma__history';
+        $cfg['Servers'][$i]['recent'] = 'pma__recent';
+        $cfg['Servers'][$i]['favorite'] = 'pma__favorite';
+        $cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
+        $cfg['Servers'][$i]['tracking'] = 'pma__tracking';
+        $cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
+        $cfg['Servers'][$i]['users'] = 'pma__users';
+        $cfg['Servers'][$i]['usergroups'] = 'pma__usergroups';
+        $cfg['Servers'][$i]['navigationhiding'] = 'pma__navigationhiding';
+        $cfg['Servers'][$i]['savedsearches'] = 'pma__savedsearches';
+        $cfg['Servers'][$i]['central_columns'] = 'pma__central_columns';
+        $cfg['Servers'][$i]['designer_settings'] = 'pma__designer_settings';
+        $cfg['Servers'][$i]['export_templates'] = 'pma__export_templates';
     }
     if (isset($_ENV['PMA_CONTROLHOST'])) {
-      $cfg['Servers'][$i]['controlhost'] = $_ENV['PMA_CONTROLHOST'];
+        $cfg['Servers'][$i]['controlhost'] = $_ENV['PMA_CONTROLHOST'];
     }
     if (isset($_ENV['PMA_CONTROLPORT'])) {
-      $cfg['Servers'][$i]['controlport'] = $_ENV['PMA_CONTROLPORT'];
+        $cfg['Servers'][$i]['controlport'] = $_ENV['PMA_CONTROLPORT'];
     }
     if (isset($_ENV['PMA_CONTROLUSER'])) {
-      $cfg['Servers'][$i]['controluser'] = $_ENV['PMA_CONTROLUSER'];
+        $cfg['Servers'][$i]['controluser'] = $_ENV['PMA_CONTROLUSER'];
     }
     if (isset($_ENV['PMA_CONTROLPASS'])) {
-      $cfg['Servers'][$i]['controlpass'] = $_ENV['PMA_CONTROLPASS'];
+        $cfg['Servers'][$i]['controlpass'] = $_ENV['PMA_CONTROLPASS'];
     }
     $cfg['Servers'][$i]['compress'] = false;
     $cfg['Servers'][$i]['AllowNoPassword'] = true;
--- launch.py
+++ launch.py
@@ -3,6 +3,7 @@ import shutil
 import subprocess
 import time
 
+
 def main():
     # Couleurs pour les messages (non directement nécessaires dans Python mais émulation via ANSI codes)
     RED = "\033[0;31m"
@@ -13,24 +14,28 @@ def main():
     # Initialisation de la configuration de l'application web
     print("##########")
     print("Initialisation de la configuration de l'application web")
-    
+
     os.chdir("webApp")
-    if not os.path.exists("config.php"):
-        shutil.copy("config.php.example", "config.php")
-        print(f"{GREEN}Update docker/docker-compose.yml and webApp/config.php files with new password.{NC}")
+    if not os.path.exists("db_connection/config.php"):
+        shutil.copy("db_connection/config.php.example", "db_connection/config.php")
+        print(
+            f"{GREEN}Update docker/docker-compose.yml and webApp/db_connection/config.php files with new password.{NC}"
+        )
         input("Then press any key to continue... ")
-    
-    if os.path.exists("config.php.example") and os.path.exists("config.php"):
-        if filecmp("config.php.example", "config.php"):
+
+    if os.path.exists("db_connection/config.php.example") and os.path.exists(
+        "db_connection/config.php"
+    ):
+        if filecmp("db_connection/config.php.example", "db_connection/config.php"):
             print(f"{RED}WARNING default password seems to be used !!!{NC}")
-    
+
     os.chdir("..")
 
     ##########
     # Création du répertoire de données initiales
     print("##########")
     print("Création du répertoire de données initiales")
-    
+
     # Création du répertoire de données initiales s'il n'existe pas
     try:
         os.makedirs("db/data", exist_ok=False)
@@ -51,10 +56,10 @@ def main():
                 # Copie le fichier
                 shutil.copy(source_path, target_path)
                 print(f"Copied: {source_path} -> {target_path}")
-            
+
     except OSError as error:
         print(f"{GREEN}Data already exist!{NC}")
-        
+
     # Chemin vers le fichier db/data/README
     readme_path = "db/data/README"
     # Texte à ajouter
@@ -72,38 +77,41 @@ def main():
     # Lancement de Docker
     print("##########")
     print("Lancement de Docker")
-    
+
     os.chdir("docker")
     if not os.path.exists("docker-compose.yml"):
         shutil.copy("docker-compose.yml.example", "docker-compose.yml")
-        print(f"{GREEN}Update docker/docker-compose.yml and webApp/config.php files with new password.{NC}")
+        print(
+            f"{GREEN}Update docker/docker-compose.yml and webApp/config.php files with new password.{NC}"
+        )
         input("Then press any key to continue... ")
 
-    if os.path.exists("docker-compose.yml.example") and os.path.exists("docker-compose.yml"):
+    if os.path.exists("docker-compose.yml.example") and os.path.exists(
+        "docker-compose.yml"
+    ):
         if filecmp("docker-compose.yml.example", "docker-compose.yml"):
             print(f"{RED}WARNING default password seems to be used !!!{NC}")
 
-    
-    if os.name == 'nt':
-        #prog = subprocess.Popen(['runas', '/noprofile', '/user:Administrator', 'docker-compose up'],stdin=subprocess.PIPE)
-        #prog.stdin.write(b'password')
-        prog = subprocess.Popen(['docker-compose', 'up'])
+    if os.name == "nt":
+        # prog = subprocess.Popen(['runas', '/noprofile', '/user:Administrator', 'docker-compose up'],stdin=subprocess.PIPE)
+        # prog.stdin.write(b'password')
+        prog = subprocess.Popen(["docker-compose", "up"])
         prog.communicate()
-    else:    
+    else:
         subprocess.run(["sudo", "docker-compose", "up"], check=True)
 
     # Pause pour laisser Docker démarrer
     time.sleep(5)
 
-    if os.name == 'nt':
-        #prog = subprocess.Popen(['runas', '/noprofile', '/user:Administrator', 'docker-compose ps'],stdin=subprocess.PIPE)
-        #prog.stdin.write(b'password')
-        prog = subprocess.Popen(['docker-compose', 'ps'])
-        #prog.stdin.write(b'password')
+    if os.name == "nt":
+        # prog = subprocess.Popen(['runas', '/noprofile', '/user:Administrator', 'docker-compose ps'],stdin=subprocess.PIPE)
+        # prog.stdin.write(b'password')
+        prog = subprocess.Popen(["docker-compose", "ps"])
+        # prog.stdin.write(b'password')
         prog.communicate()
-    else:            
+    else:
         subprocess.run(["sudo", "docker-compose", "ps"], check=True)
-        
+
     os.chdir("..")
 
     # Population avec des données libres
@@ -112,10 +120,12 @@ def main():
     # Population via ADE
     # subprocess.run(["python3", "ade2sql.py"], check=True)
 
+
 def filecmp(file1, file2):
     """Compare deux fichiers pour vérifier s'ils sont identiques."""
     with open(file1, "r") as f1, open(file2, "r") as f2:
         return f1.read() == f2.read()
+
 
 if __name__ == "__main__":
     main()
