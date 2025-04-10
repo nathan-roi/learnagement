6cc513598b6ec21cfe03feacd92b7d4331486c01
FIMAlexSender <FIM.Alex.Sender@gmail.com>
2025-04-10 08:45:48 +0200

    Add logo

--- webappnext/app/homepage/competences/filieres.tsx
+++ webappnext/app/homepage/competences/filieres.tsx
@@ -1,42 +1,70 @@
 import axios from "axios";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { useUserInfosStore } from "@/app/store/useUserInfosStore";
 
 import ModaleFiliere from "@/app/homepage/competences/modaleFiliere";
+import polytechLogo from "@/app/images/Logo_Reseau_Polytech.svg";
+import usmbLogo from "@/app/images/Logo_USMB_web_grand_RVB.png";
 
-export default function Filieres(){
-    const {user} = useUserInfosStore()
-    const [nomFilieres, setNomFilieres] = useState<string[]>([])
-    const [nomFiliereClicked, setNomFiliereClicked] = useState([])
+export default function Filieres() {
+  const { user } = useUserInfosStore();
+  const [nomFilieres, setNomFilieres] = useState<string[]>([]);
+  const [nomFiliereClicked, setNomFiliereClicked] = useState([]);
 
-    useEffect(() => {
-        let form_data = new FormData()
-        form_data.append("userId", user.userId)
-        axios.post("http://localhost:8080/select/selectFilieres.php", form_data)
-            .then(response => {
-                setNomFilieres(response.data)
-            })
-    }, []);
-    function filiereClicked(str: string){
-        let res:any = nomFilieres.find((element: string): boolean => str == element[0])
-        if(res != undefined){
-            setNomFiliereClicked(res)
-        }
+  useEffect(() => {
+    let form_data = new FormData();
+    form_data.append("userId", user.userId);
+    axios
+      .post("http://localhost:8080/select/selectFilieres.php", form_data)
+      .then((response) => {
+        setNomFilieres(response.data);
+      });
+  }, []);
+
+  function filiereClicked(str: string) {
+    let res: any = nomFilieres.find(
+      (element: string): boolean => str == element[0]
+    );
+    if (res != undefined) {
+      setNomFiliereClicked(res);
     }
+  }
 
-    return (
-        <>
-            <div id="cards-container" className={"w-full px-3 flex flex-wrap justify-center gap-10 gap-y-8"}>
-                {nomFilieres.length > 0 ? (
-                    nomFilieres.map((nom: string, index: number) =>
-                        <div key={index} onClick={(event:any): void => {filiereClicked(event.target.innerText)}} className={"w-60 h-16 flex justify-center items-center rounded-lg shadow-default cursor-pointer"}>
-                            <p className={"font-semibold"}>{nom[0]}</p>
-                        </div>)
-                ) : (
-                    <p>Aucune filière disponible.</p>
-                )}
+  return (
+    <>
+      <div
+        id="cards-container"
+        className={"w-full px-3 flex flex-wrap justify-center gap-10 gap-y-8"}
+      >
+        {nomFilieres.length > 0 ? (
+          nomFilieres.map((nom: string, index: number) => (
+            <div
+              key={index}
+              onClick={(event: any): void => {
+                filiereClicked(event.target.innerText);
+              }}
+              className={
+                "w-60 h-16 flex justify-start items-center rounded-lg shadow-default cursor-pointer gap-2 px-2"
+              }
+            >
+              <img
+                src={nom[0] === "POLYTECH" ? polytechLogo.src : usmbLogo.src}
+                alt="logo"
+                className="w-8 h-8 object-contain"
+              />
+              <p className={"font-semibold"}>{nom[0]}</p>
             </div>
-            {nomFiliereClicked.length > 0 && <ModaleFiliere nomFiliere={nomFiliereClicked} setNomFiliere={setNomFiliereClicked}/>}
-        </>
-    )
+          ))
+        ) : (
+          <p>Aucune filière disponible.</p>
+        )}
+      </div>
+      {nomFiliereClicked.length > 0 && (
+        <ModaleFiliere
+          nomFiliere={nomFiliereClicked}
+          setNomFiliere={setNomFiliereClicked}
+        />
+      )}
+    </>
+  );
 }
